a54027977a7bfe87764b4b4b10d52341ae972f0c
dldemoks <demokstar@gmail.com>
2017-02-20 22:20:49 +0700

    update

--- payeer/controllers/front/status.php
+++ payeer/controllers/front/status.php
@@ -24,17 +24,17 @@ class PayeerStatusModuleFrontController extends ModuleFrontController
 
 			$log_text = 
 				"--------------------------------------------------------\n" .
-				"operation id		" . Tools::getValue('m_operation_id') . "\n" .
-				"operation ps		" . Tools::getValue('m_operation_ps') . "\n" .
-				"operation date		" . Tools::getValue('m_operation_date') . "\n" .
-				"operation pay date	" . Tools::getValue('m_operation_pay_date') . "\n" .
-				"shop				" . Tools::getValue('m_shop') . "\n" .
-				"order id			" . Tools::getValue('m_orderid') . "\n" .
-				"amount				" . Tools::getValue('m_amount') . "\n" .
-				"currency			" . Tools::getValue('m_curr') . "\n" .
-				"description		" . base64_decode(Tools::getValue('m_desc')) . "\n" .
-				"status				" . Tools::getValue('m_status') . "\n" .
-				"sign				" . Tools::getValue('m_sign') . "\n\n";
+				"operation id       " . Tools::getValue('m_operation_id') . "\n" .
+				"operation ps       " . Tools::getValue('m_operation_ps') . "\n" .
+				"operation date     " . Tools::getValue('m_operation_date') . "\n" .
+				"operation pay date " . Tools::getValue('m_operation_pay_date') . "\n" .
+				"shop               " . Tools::getValue('m_shop') . "\n" .
+				"order id           " . Tools::getValue('m_orderid') . "\n" .
+				"amount             " . Tools::getValue('m_amount') . "\n" .
+				"currency           " . Tools::getValue('m_curr') . "\n" .
+				"description        " . base64_decode(Tools::getValue('m_desc')) . "\n" .
+				"status             " . Tools::getValue('m_status') . "\n" .
+				"sign               " . Tools::getValue('m_sign') . "\n\n";
 			
 			$log_file = Configuration::get('payeer_log');
 			
@@ -92,10 +92,10 @@ class PayeerStatusModuleFrontController extends ModuleFrontController
 			{
 				// загрузка заказа
 				
-				$cart = new Cart(intval($m_orderid));
-				$order_curr = new Currency(intval($cart->id_currency));
+				$order = new Order((int)$m_orderid);
+				$order_curr = new Currency($order->id_currency);
 				$order_curr = $order_curr->iso_code == 'RUR' ? 'RUB' : $order_curr->iso_code;
-				$order_amount = number_format($cart->getOrderTotal(true, Cart::BOTH), 2, '.', '');
+				$order_amount = number_format($order->total_paid, 2, '.', '');
 
 				// проверка суммы и валюты
 			
@@ -110,26 +110,39 @@ class PayeerStatusModuleFrontController extends ModuleFrontController
 					$message .= $payeer->l(' - wrong currency', 'status') . "\n";
 					$err = true;
 				}
-				
+
 				// проверка статуса
 				
 				if (!$err)
 				{
+					$history = new OrderHistory();
+					$history->id_order = (int)$m_orderid;
+					
 					switch (Tools::getValue('m_status'))
 					{
 						case 'success':
-							$orderStatusId = Configuration::get('PS_OS_PAYMENT');
+							
+							if ($order->current_state != Configuration::get('PS_OS_PAYMENT'))
+							{
+								$orderStatusId = Configuration::get('PS_OS_PAYMENT');
+								$history->changeIdOrderState((int)($orderStatusId), $order);
+							}
+							
 							break;
 							
 						default:
+						
 							$message .= $payeer->l(' - the payment status is not success', 'status') . "\n";
-							$orderStatusId = Configuration::get('PS_OS_ERROR');
 							$err = true;
+							
+							if ($order->current_state != Configuration::get('PS_OS_ERROR'))
+							{
+								$orderStatusId = Configuration::get('PS_OS_ERROR');
+								$history->changeIdOrderState((int)($orderStatusId), $order);
+							}
+							
 							break;
 					}
-					
-					$customer = new Customer($cart->id_customer);
-					$this->module->validateOrder($m_orderid, $orderStatusId, $order_amount, $payeer->displayName, null, array(), null, false, $customer->secure_key);
 				}
 			}
 			
